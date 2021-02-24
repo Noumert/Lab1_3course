67663955c2df332778461756d1b2e205d7140310
vladmir.pavluchinskiy <vladmir.pavluchinskiy@gmail.com>
2021-02-24 10:41:42 +0200

    NoCrit

--- src/main/java/ExceptionManager.java
+++ src/main/java/ExceptionManager.java
@@ -6,6 +6,15 @@ public class ExceptionManager {
     public final List criticalExceptions = Arrays.asList(NullPointerException.class,NumberFormatException.class,IllegalArgumentException.class);
 
     private int critExcCount = 0;
+    private  int noCritExcCount = 0;
+
+    public int getNoCritExcCount() {
+        return noCritExcCount;
+    }
+
+    public void setNoCritExcCount(int noCritExcCount) {
+        this.noCritExcCount = noCritExcCount;
+    }
 
     public int getCritExcCount() {
         return critExcCount;
@@ -22,6 +31,8 @@ public class ExceptionManager {
     public void countCritExc(Exception e){
         if(isCritExc(e)){
             this.critExcCount++;
+        }else {
+            this.noCritExcCount++;
         }
     }
 }
--- src/test/java/countCritExc.java
+++ src/test/java/countCritExc.java
@@ -18,35 +18,38 @@ public class countCritExc {
                                     new NumberFormatException(),
                                     new IllegalArgumentException(),
                                     new NumberFormatException(),
-                                    new IllegalArgumentException()}, 5},
+                                    new IllegalArgumentException()}, 5,0},
                     {new Exception[]
                             {new NullPointerException(),
                                     new ArithmeticException(),
                                     new ClassCastException(),
                                     new NumberFormatException(),
-                                    new IllegalArgumentException()}, 3},
+                                    new IllegalArgumentException()}, 3,2},
                     {new Exception[]
                             {new ClassCastException(),
                                     new ArithmeticException(),
                                     new ClassCastException(),
                                     new ArithmeticException(),
-                                    new IllegalArgumentException()}, 1},
+                                    new IllegalArgumentException()}, 1,4},
                     {new Exception[]
                             {new ClassCastException(),
                                     new ArithmeticException(),
                                     new ClassCastException(),
                                     new ArithmeticException(),
-                                    new ClassCastException()}, 0}
+                                    new ClassCastException()}, 0,5}
             });
         }
 
         private Exception[] input;
 
-        private int expected;
+        private int expected1;
 
-        public countCritExc(Exception[] input, int expected) {
+        private int expected2;
+
+        public countCritExc(Exception[] input, int expected1, int expected2) {
             this.input = input;
-            this.expected = expected;
+            this.expected1 = expected1;
+            this.expected2 = expected2;
         }
 
     @Test
@@ -55,6 +58,7 @@ public class countCritExc {
         for (Exception e: input) {
             exceptionManager.countCritExc(e);
         }
-        assertEquals(expected, exceptionManager.getCritExcCount());
+        assertTrue(expected1 == exceptionManager.getCritExcCount() && expected2 == exceptionManager.getNoCritExcCount());
+//        assertEquals(expected, exceptionManager.getCritExcCount());
     }
 }
